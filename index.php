da45912fd0696b9a717b7fffc73cc02d01cf5cf7
Marc Kettler <marckettler@g.harvard.edu>
2013-09-13 22:39:19 -0400

    Started over and took the last copy of index.php from my first attempt as a reference

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>CSCI E-15 Project 1</title>
+	</head>
+	
+	<body>
+		<article>
+			<h1>Marc Kettler</h1>
+			<p>
+				As the heading above states my name is Marc Kettler.
+				I am currently the Lab Manager and a Tutor for the Mathematics 
+				and CIS Department at the SUNY College at Old Westbury.
+				I am also quite possibly the worst content creator around.
+				So for now this will suffice as I am just working out the details
+				of this project's workflow. Hopefully you will not see this in my
+				Final submission as I would like to make this page slightly better
+				than this simple paragraph.
+			</p>
+			<p>
+				The code editor I am currently using is Eclipse Kepler Release with
+				the handy dandy Egit Plugin which at this time seems more painful
+				then just simply using the command line.
+			</p>
+		</article>
+		
+	</body>
+</html>
+
