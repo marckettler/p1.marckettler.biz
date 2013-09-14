3b5aa2d72b459f0abf24e9e7d647be819a5b5f58
Marc Kettler <marckettler@g.harvard.edu>
2013-09-13 23:07:52 -0400

    Updated index.php with a better bio

--- index.php
+++ index.php
@@ -8,19 +8,18 @@
 		<article>
 			<h1>Marc Kettler</h1>
 			<p>
-				As the heading above states my name is Marc Kettler.
-				I am currently the Lab Manager and a Tutor for the Mathematics 
-				and CIS Department at the SUNY College at Old Westbury.
-				I am also quite possibly the worst content creator around.
-				So for now this will suffice as I am just working out the details
-				of this project's workflow. Hopefully you will not see this in my
-				Final submission as I would like to make this page slightly better
-				than this simple paragraph.
+				My name is Marc Kettler and I am currently the Lab Manager and a Tutor for the Mathematics and CIS Department at the SUNY College at Old Westbury.
+                I am also quite possibly the worst content creator around. With that in mind I figured it would be safe to just ramble a little bit. 
+                I suppose it is best that I mention the fact that I just got married in June. I just have this feeling that if I neglect to mention my lovely 
+                wife Sung and how important she is to me there may be dire consequences. I also better mention that Sung has the cutest dog I have ever seen.
+                If I neglect to mention Kiwi the situation will probably go from dire consequences to divorce court. If I wasn't being lazy I would put a picture
+                of the dog on this page. I guess I will just have to address the missing photo in the next commit. 
 			</p>
 			<p>
-				The code editor I am currently using is Eclipse Kepler Release with
-				the handy dandy Egit Plugin which at this time seems more painful
-				then just simply using the command line.
+				The code editor I started using was Eclipse Kepler with the Egit plug-in. That proved to be more annoying than functional for me so I have chosen
+                to switch to Adobe Dreamweaver CS5.5 and the command line. Command line and the GitHub App seem to be a better alternative at this time and Dreamweaver
+                will do for this project. I will most likely switch to PhpStorm but since at this point the project we are working on is more about the workflow I will
+                stick with what I have.
 			</p>
 		</article>
 		
