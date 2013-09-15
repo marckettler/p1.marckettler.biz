5ea5933c668e406fe9df0c0552bc7f3a3d16791f
Marc Kettler <marckettler@g.harvard.edu>
2013-09-15 15:21:59 -0400

    Added kiwi.jpg and made some minor content edits.

--- index.php
+++ index.php
@@ -10,13 +10,14 @@
 		<main class="container">
 			<header><h1>Marc Kettler</h1></header>
 			<article class="content">
-            	<p>
+            	<p><img alt="Kiwi the Sheltie" src="kiwi.jpg"></p>
+                <p>
 				My name is Marc Kettler and I am currently the Lab Manager and a Tutor for the Mathematics and CIS Department at the SUNY College at Old Westbury.
                 I am also quite possibly the worst content creator around. If I could put some lorem ipsum in for my bio it might actually be more meaningful than
                 what I can come up with on my own. With that in mind I figured it would be safe to just ramble a little bit. I suppose it is best that I mention 
                 the fact that I just got married in June. I just have this feeling that if I neglect to mention my lovely wife Sung and how important she is to me 
-                there may be dire consequences. I also better mention that Sung has the cutest dog I have ever seen. If I neglect to mention Kiwi the situation 
-                will probably go from dire consequences to divorce court. If I wasn't being lazy I would put a picture of the dog on this page. 
+                there may be dire consequences. The dog is Sung's pet Kiwi and if I neglected to put a photo of her the situation will probably go from dire consequences 
+                to divorce court.
 				</p>
 				<p>
 				The code editor I started using was Eclipse Kepler with the Egit plug-in. That proved to be more annoying than functional for me so I have chosen
@@ -26,7 +27,7 @@
 				</p>
 			</article>
             <footer>
-            	<p>All Rights would be reserved or there would be some trademark if I had one</p>
+            	<p>All Rights will be reserved or something like that... I just wanted a footer ;-)</p>
             </footer>
 		</main>
 	</body>
