9bb17ee380ea97ffa662d77ade9202e48cae9b22
Marc Kettler <marckettler@g.harvard.edu>
2013-09-15 14:44:49 -0400

    Added p1.css for some minor styling. Modified index.php to make sure it is validating.

--- index.php
+++ index.php
@@ -2,27 +2,33 @@
 <html>
 	<head>
 		<title>CSCI E-15 Project 1</title>
+        <meta charset="utf-8">
+        <link href="p1.css" rel="stylesheet" type="text/css">
 	</head>
 	
 	<body>
-		<article>
-			<h1>Marc Kettler</h1>
-			<p>
+		<main class="container">
+			<header><h1>Marc Kettler</h1></header>
+			<article class="content">
+            	<p>
 				My name is Marc Kettler and I am currently the Lab Manager and a Tutor for the Mathematics and CIS Department at the SUNY College at Old Westbury.
-                I am also quite possibly the worst content creator around. With that in mind I figured it would be safe to just ramble a little bit. 
-                I suppose it is best that I mention the fact that I just got married in June. I just have this feeling that if I neglect to mention my lovely 
-                wife Sung and how important she is to me there may be dire consequences. I also better mention that Sung has the cutest dog I have ever seen.
-                If I neglect to mention Kiwi the situation will probably go from dire consequences to divorce court. If I wasn't being lazy I would put a picture
-                of the dog on this page. I guess I will just have to address the missing photo in the next commit. 
-			</p>
-			<p>
+                I am also quite possibly the worst content creator around. If I could put some lorem ipsum in for my bio it might actually be more meaningful than
+                what I can come up with on my own. With that in mind I figured it would be safe to just ramble a little bit. I suppose it is best that I mention 
+                the fact that I just got married in June. I just have this feeling that if I neglect to mention my lovely wife Sung and how important she is to me 
+                there may be dire consequences. I also better mention that Sung has the cutest dog I have ever seen. If I neglect to mention Kiwi the situation 
+                will probably go from dire consequences to divorce court. If I wasn't being lazy I would put a picture of the dog on this page. 
+				</p>
+				<p>
 				The code editor I started using was Eclipse Kepler with the Egit plug-in. That proved to be more annoying than functional for me so I have chosen
                 to switch to Adobe Dreamweaver CS5.5 and the command line. Command line and the GitHub App seem to be a better alternative at this time and Dreamweaver
                 will do for this project. I will most likely switch to PhpStorm but since at this point the project we are working on is more about the workflow I will
                 stick with what I have.
-			</p>
-		</article>
-		
+				</p>
+			</article>
+            <footer>
+            	<p>All Rights would be reserved or there would be some trademark if I had one</p>
+            </footer>
+		</main>
 	</body>
 </html>
 
